65838dbbb6049d68d7dca27bc34e31c3562ecd42
jako <jan.kodym@post.cz>
2024-07-10 16:39:30 +0200

    Nahrazeni st.experimental_rerun() za st.rerun()

--- web.py
+++ web.py
@@ -26,7 +26,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input(label="Enter a todo:", label_visibility="hidden", placeholder="Add new todo...",
               on_change=add_todo, key='new_todo')
